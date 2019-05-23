65eb5dfeaa7da10ce6a8d3a16dcfc12952452b96
amitchhatbarbacancy <amit.chhatbar@bacancytechnology.com>
2019-05-23 11:44:55 +0530

    removed consoles and unused onClick fn.

--- src/App.js
+++ src/App.js
@@ -2,26 +2,19 @@
 import React, {Component} from 'react';
 import './App.css';
 import jsonData from './inputJson';
-import { isEmpty, isNull } from 'lodash';
 
 class App extends Component {
   state = {
   }
 
-  onParentClick(cNode) {
-    console.log(`@@@@@@@@@@@@@@@@@@@`, cNode)
-  }
-
   currentNode = (node) => jsonData.layers.filter(cNode => cNode.parentLayerId === node).map(cNode => (
   	<ul key={`node_${cNode.id}`}>
-    { cNode.parentLayerId === -1 && <li onClick={(cNode) =>this.onParentClick(cNode)}>{cNode.name}</li> }
-    { cNode.parentLayerId !== -1 && <li>{cNode.name}</li> }
+    <li>{cNode.name}</li>
       {this.currentNode(cNode.id)}
     </ul>
   ))
 
   render(){
-    console.log("$$$$$$$$$$$$$", this.state)
     return (
       <div className="App">
         <div>
